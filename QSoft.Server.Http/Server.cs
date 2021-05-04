e0d3ec8c9789b9d14bde83869189aee479cf1c12
oven425 <oven425@yahoo.com.tw>
2021-05-04 18:09:07 +0800

    update

--- QSoft.Server.Http/Server.cs
+++ QSoft.Server.Http/Server.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,62 @@ namespace QSoft.Server.Http
 {
     public class Server
     {
+        HttpListener m_Listener = new HttpListener();
+        public void Start(string ip, int port)
+        {
+            //HttpListener listener = new HttpListener();
+            //listener.Prefixes.Add("http://127.0.0.1:3456");
+            //listener.AuthenticationSchemes = AuthenticationSchemes.Digest;
+            //listener.Realm = "testrealm@host.com";
+            //listener.Start();
+            //var context = listener.GetContext();
+
+            string domain = Environment.UserDomainName;
+            string hostname = Dns.GetHostName();
+            this.m_Listener.Prefixes.Add($"http://{ip}:{port}/");
+            //this.m_Listener.AuthenticationSchemeSelectorDelegate = new AuthenticationSchemeSelector(AuthenticationSchemeForClient);
+            this.m_Listener.Realm = "testrealm@host.com";
+            this.m_Listener.AuthenticationSchemes = AuthenticationSchemes.Digest;
+            this.m_Listener.Start();
+
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var req = await this.m_Listener.GetContextAsync();
+                    if (req.Request.HttpMethod.ToUpperInvariant() == "GET" && this.m_Gets.ContainsKey(req.Request.Url.LocalPath) == true)
+                    {
+                        if (this.m_Gets[req.Request.RawUrl](req) == true)
+                        {
+
+                        }
+                    }
+                }
+            });
+        }
+
+        AuthenticationSchemes AuthenticationSchemeForClient(HttpListenerRequest request)
+        {
+            // Do not authenticate local machine requests.
+            if (request.RemoteEndPoint.Address.Equals(IPAddress.Loopback))
+            {
+                return AuthenticationSchemes.Digest;
+            }
+            else
+            {
+                return AuthenticationSchemes.IntegratedWindowsAuthentication;
+            }
+        }
+
+        ConcurrentDictionary<string, Func<HttpListenerContext, bool>> m_Gets = new ConcurrentDictionary<string, Func<HttpListenerContext, bool>>();
+        public void Get(string path, Func<HttpListenerContext, bool> data)
+        {
+            this.m_Gets.AddOrUpdate(path, data, (k, v) => data);
+        }
+    }
+
+    public class Session
+    {
+        public string Path { set; get; }
     }
 }
--- WPF_Http_Server/MainWindow.xaml.cs
+++ WPF_Http_Server/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using QSoft.Server.Http;
 
 namespace WPF_Http_Server
 {
@@ -23,6 +25,31 @@ namespace WPF_Http_Server
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Server server = new Server();
+            try
+            {
+                server.Get("/index", (HttpListenerContext data) => 
+                {
+                    return false;
+                });
+                server.Get("/test", (HttpListenerContext data) =>
+                {
+                    string str = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.ffff");
+                    byte[] buf = Encoding.UTF8.GetBytes(str);
+                    data.Response.ContentLength64 = buf.Length;
+                    data.Response.OutputStream.Write(buf, 0, buf.Length);
+                    return false;
+                });
+                server.Start("127.0.0.1", 3456);
+            }
+            catch(Exception ee)
+            {
+                System.Diagnostics.Trace.WriteLine(ee.Message);
+            }
         }
     }
 }
