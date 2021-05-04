19177e530ef25301aad89ed163cc3f6e6c1fe2b0
oven425 <oven425@yahoo.com.tw>
2021-05-04 14:21:46 +0800

    update

---
+++ QSoft.Server.Http/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSoft.Server.Http
+{
+    public class Server
+    {
+    }
+}
